4234b5d3250954076ac89f77df4b3c0a192a15a4
ABDULQUADRI <shittuabdquadri0@gmail.com>
2024-05-17 06:36:01 -0700

    creator components

--- src/Ui/Creator.jsx
+++ src/Ui/Creator.jsx
@@ -1,7 +1,81 @@
 import "./Creator.css"
 import React from 'react'
 import { FaRocket } from "react-icons/fa"
+import Man from "../assets/MAn.png"
+import Girl from "../assets/GirlNft.png"
+import SecondGirl from "../assets/Girl.png"
+import MarkFace from "../assets/MarkFace.png"
+import Cat from "../assets/Cat.png"
+import Shroomie from "../assets/Shroomie.png"
+import Robot from "../assets/Robot.png"
+import Rusty from "../assets/RustyRobot.png"
+import AnimaKid from "../assets/Animakid.png"
+import Woman from "../assets/Woman.png"
+import ChinaMan from "../assets/ChinaMan.png"
 const Creator = () => {
+  const CreatorCOllection=[
+    { 
+       creatorImg:Woman,
+       creatorName: "Keepitreal",
+       sales: 34.63,
+    },
+    {
+      creatorImg:Man,
+      creatorName: "digilab",
+      sales: 34.63,
+    },
+    {
+      creatorImg:SecondGirl,
+      creatorName: "Juanie",
+      sales: 34.63,
+    },
+    {
+      creatorImg:Girl,
+      creatorName: "Gravityone",
+      sales: 34.63,
+    },
+    {
+      creatorImg:MarkFace,
+      creatorName:"Bluewhale",
+      sales: 34.63
+    },
+    {
+      creatorImg:Cat,
+      creatorName:"Mr fox",
+      sales: 34.63
+    },
+    {
+      creatorImg:Shroomie,
+      creatorName:Shroomie,
+      sales: 34.63
+    },
+    {
+        creatorImg:Robot,
+        creatorName:"Robotica",
+        sales: 34.63
+    },
+    {
+      creatorImg:Rusty,
+      creatorName:"Rusty Robot",
+      sales: 34.63
+     },
+     {
+        creatorImg:AnimaKid,
+        creatorName:"Animakid",
+        sales: 34.63
+     },
+     {
+        creatorImg:Woman,
+        creatorName:"Dotgu",
+        sales: 34.63
+     },
+     {
+        creatorImg:ChinaMan,
+        creatorName:"Gihibla",
+        sales: 34.63
+     }
+    
+  ]
   return (
     <div>
        <h3 className="top">top Creator</h3>
@@ -9,6 +83,18 @@ const Creator = () => {
         <p className="chkt">Checkout Top Rated Creators on the NFT Marketplace</p>
         <button className="RocketBtn"> <FaRocket className="rkct"/>view ranking</button>
         </div>
+         <div className="Grid">
+          {
+            CreatorCOllection.map((creatorInfo , index)=>{
+              return <div className="creatorInfoBox" key={index}> 
+                <img src={creatorInfo.creatorImg} alt=""  draggable="false" className="creatorImg"/>
+               <p className="creatorName">{creatorInfo.creatorName}</p>
+                <p className="sales">total sales : {creatorInfo.sales} <span>ETH</span></p>
+              </div>
+              
+            })
+          }
+          </div>Grid
     </div>
   )
 }
