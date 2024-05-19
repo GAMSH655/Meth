ffe0d02b5f1c8a9a7a4e53168e31f76696e75fa8
ABDULQUADRI <shittuabdquadri0@gmail.com>
2024-05-19 07:19:12 -0700

    created another component inside the UI folder and render it to the home folder

---
+++ src/Ui/Categories.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import Brush from"../assets/Brush.png"
+import Holder from  "../assets/Holder.png"
+import Music from "../assets/Music.png"
+import Video from "../assets/Video.png"
+import Ball from "../assets/Ball.png"
+import Utillity from "../assets/Utillity.png"
+import World from "../assets/World.png"
+// import Camera from '../assets/Camera.png  '
+import "./Categories.css"
+const Categories = () => {
+    const CategoriesCollection= [
+        {
+        categoriesImg: Brush,
+        categoriesName :"art"
+        }, 
+        {
+        categoriesImg: Holder,
+        categoriesName :"Collectibles"
+        }, 
+        {
+        categoriesImg: Music,
+        categoriesName :"Music"
+        },
+        // {
+        // categoriesImg: Camera,
+        // categoriesName :"photography"
+        // },
+        {
+        categoriesImg: Video,
+        categoriesName :"Video"
+        },
+        {
+        categoriesImg:Utillity,
+        categoriesName :"Utillity"
+        },
+        {
+        categoriesImg:Ball,
+        categoriesName :"sport"
+        },
+        {
+        categoriesImg:World,
+        categoriesName :"virtual world"
+        },
+    ]
+  return (
+    <div>
+        <div>
+            <h3 className="browse">browse categories</h3>
+            <div className="categoriesInnerContainerGrid">
+                {
+                    CategoriesCollection.map((categInfo ,index) =>{
+                        return <div key={index} className='categInfo_div'>
+                            <img src={categInfo.categoriesImg} className='categoriesImg'/>
+                            <p className='categoriesName'>{categInfo.categoriesName}</p>
+                        </div>
+                    })
+                }
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Categories
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from "../Ui/Hero"
 import Trending from "../Ui/Trending"
 import Creator from "../Ui/Creator"
+import Categories from '../Ui/Categories'
 export const Home = () => {
   return (
     <div>
        <Hero/>
        <Trending/>
        <Creator/>
+       <Categories/>
     </div>
   )
 }
