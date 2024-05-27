fceab9f86bc26add6d3ca68c397c4e1f75c3c340
ABDULQUADRI <shittuabdquadri0@gmail.com>
2024-05-27 10:26:22 -0700

    working in the footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Home from "../src/pages/Home"
-
+import Footer from "../src/Components/Footer"
 function App() {
   const [count, setCount] = useState(0)
 
@@ -9,6 +9,7 @@ function App() {
     <>
       <>
       <Home/>
+      <Footer/>
       </>
     </>
   )
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { CiShop } from "react-icons/ci";
+import { FaDiscord , FaYoutube , FaTwitter, FaInstagram , FaEnvelope } from 'react-icons/fa';
+import "./Footer.css"
+const Footer = () => {
+  return (
+    <div>
+       <footer>
+         <div className="footer1">
+         <h3 className="FooterlogoText"> < CiShop  className='headerIcon'/> nft marketplace </h3>
+         <p  className="NftP">NFT marketplace UI created with Anima for Figma.</p>
+         <div className="joinCon">
+            <h3 className="NftP">join our community</h3>
+            <FaDiscord className="NftP"/>  <FaYoutube className="NftP"/>  <FaTwitter className="NftP"/>  <FaInstagram className="NftP" />
+         </div>
+         </div>
+         <div className="footer2">
+         <h3 className="FooterlogoText"> Explore </h3>
+         <p className="NftP">marketplace</p>
+         <p className="NftP">Rankings</p>
+         <p className="NftP">connect wallet</p>
+         </div>
+         <div className="footer1">
+         <h3 className="FooterlogoText">join our weekly</h3>
+         <p className="NftP">Get exclusive promotions & updates straight to your inbox.</p>
+         <form action="" className='formWork'>
+             <input type="text"  placeholder='enter your mail here ' className='WorkInput'/>
+             <button className="formBTnShow"> <FaEnvelope className='envelope'/> suscribe</button>
+             <br /><br />
+              <div className="buttonFormWork">
+              <button className="formBTnSNone"> <FaEnvelope className='envelope'/> suscribe</button>
+              </div>
+             </form> 
+         </div>
+       </footer>
+    </div>
+  )
+}
+
+export default Footer
