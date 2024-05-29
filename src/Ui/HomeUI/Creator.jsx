240a00694ea9246d70eddfdcca3d6e26a6cbd1d7
ABDULQUADRI <shittuabdquadri0@gmail.com>
2024-05-29 06:37:17 -0700

    fixed rollup error

--- src/Ui/HomeUI/Creator.jsx
+++ src/Ui/HomeUI/Creator.jsx
@@ -2,7 +2,7 @@ import "./Creator.css"
 import React from 'react'
 import { FaRocket} from "react-icons/fa"
 import { FaEthereum } from "react-icons/fa";
-import Man from "@/assets/Man.png"
+//import Man from "../assets/Man.png"
 import Girl from  "../../assets/GirlNft.png"
 import SecondGirl from "../../assets/Girl.png"
 //import MarkFace from "../../assets/MarkFace.png"
@@ -21,7 +21,8 @@ const Creator = () => {
        sales: 34.63,
     },
     {
-      creatorImg:Man,
+      creatorImg:'',
+      // creatorImg:Man,
       creatorName:"digilab",
       sales: 34.63,
     },
