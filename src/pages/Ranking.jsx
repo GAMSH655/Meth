1ae01a17d394c6bae150493cddf2f2e021635849
ABDULQUADRI <shittuabdquadri0@gmail.com>
2024-05-29 03:57:13 -0700

    ddd

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -10,6 +10,23 @@ const Ranking = () => {
          <h3 className="topCrt"> top creator</h3>
          <p className="chekOut"> Check out top ranking NFT artists on the NFT Marketplace.</p>
        </div>
+              <table className='table'>
+          <tr>
+            <th>Company</th>
+            <th>Contact</th>
+            <th>Country</th>
+          </tr>
+          <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>Maria Anders</td>
+            <td>Germany</td>
+          </tr>
+          <tr>
+            <td>Centro comercial Moctezuma</td>
+            <td>Francisco Chang</td>
+            <td>Mexico</td>
+          </tr>
+        </table>
     </div>
   )
 }
