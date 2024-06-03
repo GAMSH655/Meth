9404e57957bcb09c3d8a048c0908591c3dc5b6af
ABDULQUADRI <shittuabdquadri0@gmail.com>
2024-06-03 04:55:54 -0700

    wew

--- src/pages/MarketPlace.jsx
+++ src/pages/MarketPlace.jsx
@@ -1,26 +1,85 @@
 import React from 'react'
 import "./Market.css"
 import Header from "../Components/Headers"
-import MagicMush from "../assets/Mushroom.png"
-import HappyRobot from "../assets/Robot.png"
-import HappyRob from "../assets/RobotNft.png"
-import Rusty from "../assets/RustyRobot.png"
+import MagicMush from "../assets/Mush2.png"
+import HappyRobot from "../assets/Rusty1.png"
+import HappyRob from "../assets/Rusty2.png"
+import Rob from "../assets/Rusty1.png"
+import DancingRobot from "../assets/DancingRob.png"
 import Cat from "../assets/Cat.png"
 import Bear from "../assets/Bear.png"
+import Dog from "../assets/Dog2.png"
+import Flower from "../assets/FlowerGirl.png"
+import MoonDancer from "../assets/Moon.png"
+import Travel from "../assets/Travel.png"
+import Neb from "../assets/Nebula.png"
+import Sunset from "../assets/Sunset.png"
+import Anim from "../assets/Guy.png"
+import Skeleton from "../assets/Skeleton.png"
+import Ape from "../assets/Ape.png"
+import ColorDog from "../assets/ColorDog.png"
+import Footer from "../Components/Footer"
 const MarketPlace = () => {
-  
   const MarketNftCollections = [
     { MarketNftImg:MagicMush,
       MarketNftName : "magic mushroom o325",
-      MarketNftArtist : Cat
+      MarketNftArtist : Cat,
+      ArtistNAme: "Shroomie"
     },
     { MarketNftImg:HappyRobot,
       MarketNftName : "Happy Robot 032",
-      MarketNftArtist : Cat
+      MarketNftArtist : Cat,
+      ArtistNAme: "Shroomie"
     },
-    { MarketNftImg:Rusty,
+    { MarketNftImg:HappyRob,
       MarketNftName : "Happy Robot 024",
-      MarketNftArtist : Cat
+      MarketNftArtist : Cat,
+      ArtistNAme: "Shroomie"
+    },
+    { MarketNftImg:Bear,
+      MarketNftName : "designer bear",
+      MarketNftArtist : Cat,
+      ArtistNAme: "Shroomie"
+    },
+    { MarketNftImg:Dog,
+      MarketNftName : "colorful dog 0356",
+      MarketNftArtist : Cat,
+      ArtistNAme: "Shroomie"
+    },
+    { MarketNftImg:DancingRobot,
+      MarketNftName : "Dancing Robot 0312",
+      MarketNftArtist :Rob,
+      ArtistNAme: "Robotica"
+    },
+    { MarketNftImg:Flower,
+      MarketNftName : "cherry blossom girl",
+      MarketNftArtist :MoonDancer,
+      ArtistNAme: "MoonDancer"
+    },
+    { MarketNftImg:Travel,
+      MarketNftName : "space travel",
+      MarketNftArtist :Neb,
+      ArtistNAme: "Nebula kid"
+    },
+    { MarketNftImg:Travel,
+      MarketNftName : "space travel",
+      MarketNftArtist :Neb,
+      ArtistNAme: "Nebula kid"
+    },
+    { MarketNftImg:Skeleton,
+      MarketNftName : "Catch 22",
+      MarketNftArtist :Anim,
+      ArtistNAme: "Nebula kid"
+    },
+    { MarketNftImg:Ape,
+      MarketNftName : "iceCream Ape 024",
+      MarketNftArtist :Ape,
+      ArtistNAme: "ice ape club"
+    },
+    { MarketNftImg:ColorDog,
+      MarketNftName : "colorful dog 0334",
+      MarketNftArtist :ColorDog,
+      ArtistNAme: "Puppy power"
     },
   ]
   return (
@@ -35,18 +94,31 @@ const MarketPlace = () => {
         <p  className='numP'>collections <span className='numspan'>302</span></p>
         <p className='numP2'>Nft <span className='numspan'>302</span></p>
       </div>
-      <div className="GridContainer" >
-       <div className="gridContainer">
+       <div className="MarketGrid">
         {
           MarketNftCollections.map((MarketNftCollectionInfo , index)=>{
-            return <div key={index}>
-               <img src={MarketNftCollectionInfo.MarketNftImg} alt="" />
+            return <div key={index} className='MarketNftCollectionInfo'>
+               <img src={MarketNftCollectionInfo.MarketNftImg} alt="" className='MarketNftImg'/>
                <h3 className="martketTitle">{MarketNftCollectionInfo.MarketNftName}</h3>
+               <div className="NAAme">
+              <img src={MarketNftCollectionInfo.MarketNftArtist}  className='tinyImg'  alt="" />
+              <p className="DiscoverTitle">{MarketNftCollectionInfo.ArtistNAme}</p>
+               </div>
+               <table className='tableDiscover'> 
+                <tr>
+                  <th className='thDiscover'>Price</th>
+                  <th className='thDiscover'>Highest bid</th>
+                </tr>
+                <tr>
+                  <td className='tdDiscover'>1.63 Eth</td>
+                  <td className='tdDiscover'>0.33wEth</td>
+                </tr>
+             </table>
             </div>
           })
         }
        </div>
-         </div>
+       <Footer/>
     </div>
   )
 }
